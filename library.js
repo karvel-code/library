38654ff922f8c13a31a2fe6b4c710eca2d789704
karvel-code <velgithinji@gmail.com>
2021-08-18 17:42:13 +0300

    Modify library.js

--- library.js
+++ library.js
@@ -6,3 +6,19 @@ function Book(title, author, pages, readStatus) {
   this.pages = pages;
   this.readStatus = readStatus;
 }
+
+// event: add a book
+document.querySelector("#book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  //   alert("Hello");
+  //get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const isRead = document.querySelector("#isRead").value;
+
+  //instantiate a book
+  const book = new Book(title, author, pages, isRead);
+  myLibrary.push(book);
+  console.log(book);
+});
